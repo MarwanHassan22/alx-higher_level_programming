d0d62839e5050745a02f57e0514a8eb4f4fa7940
Marwan <marwanhassan954@gmail.com>
2024-03-21 23:05:54 +0200

    .

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,3 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"{str[38:66]} {str[-22:-17]} {str[:6]}")
+
